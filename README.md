c65c5de7bb289f59cf3566be490b8bf802cbe791
Bruce Olivier <bolivierjr@gmail.com>
2018-08-06 08:23:32 -0500

    Created a base express server and used create-react-app for the frontend to start project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Discard
+
+A client-server based chat app using socket.io, react and express.
+
+## How to Install and Run App
+
+```bash
+# Clone the Repository
+git clone https:/github/bolivierjr/Discard
+
+# Change to Directory
+cd Discard
+
+# Install Dependencies
+yarn install
+
+# Start Development Server
+yarn start:dev
+
+# Build for Production
+yarn build
+
+# Start Production Server
+yarn start
+```
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.Server(app);
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '..', 'build/')));
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(404).send('it is broked');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}...`);
+});
