2c102e20805f7ce7e54a1778d76f46f3c8c1a756
Bruce Olivier <bolivierjr@gmail.com>
2018-08-17 08:43:48 -0500

    Added dotenv for environment variables. Started making the routes handle ansychrously. Adding a little more error handling.

--- server/index.js
+++ server/index.js
@@ -2,24 +2,39 @@ import compression from 'compression';
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import redis from 'redis';
+import { promisify } from 'util';
+import dotenv from 'dotenv/config';
 
 const app = express();
 const server = http.Server(app);
 const PORT = process.env.PORT || 8080;
 
+// Compress response bodies for all requests
 app.use(compression());
 
+// Serve this build directory for prod
 app.use(express.static(path.join(__dirname, '..', 'build/')));
 
-app.get('/*', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+// Catch-all route handler
+app.get(
+  '/*',
+  promisify(async (req, res, next) => {
+    res.sendFile(await path.resolve(__dirname, '..', 'build', 'index.html'));
+  })
+);
 
+// Error handler middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(404).send('it is broked');
+  if (err instanceof redis.RedisError) {
+    console.log(err.stack);
+    return res.status(500).json({ message: "it's really broked" });
+  } else if (err instanceof Error) {
+    console.log(err.stack);
+    return res.status(404).json({ message: 'It is broked' });
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}...`);
+  console.log(`Listening on http://${process.env.REACT_APP_DOMAIN}:${PORT}...`);
 });
